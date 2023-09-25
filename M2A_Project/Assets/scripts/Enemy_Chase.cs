78e8206577b372570b446ae73635e1916299eb74
CatEnjoyers <137115804+CatEnjoyers@users.noreply.github.com>
2023-09-25 17:59:19 -0400

    change in movement capabilities
    changes from four way only direction to 8-way directional movement

--- M2A_Project/Assets/scripts/Enemy_Chase.cs
+++ M2A_Project/Assets/scripts/Enemy_Chase.cs
@@ -4,35 +4,19 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player; // Reference to the player's transform.
     public float moveSpeed = 3.0f; // Adjust this value to control the speed of the enemy.
+    private Rigidbody2D rb;
 
-    private Vector2 currentDirection = Vector2.zero; // Current movement direction.
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         // Calculate the direction to move towards the player.
         Vector2 direction = (player.position - transform.position).normalized;
 
-        // Check if the current direction is diagonal and if it's time to change direction.
-        if (IsDiagonal(currentDirection) || Vector2.Dot(currentDirection, direction) <= 0)
-        {
-            // Change direction to the axis that aligns with the player's position.
-            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-            {
-                currentDirection = new Vector2(direction.x, 0);
-            }
-            else
-            {
-                currentDirection = new Vector2(0, direction.y);
-            }
-        }
-
-        // Move the enemy.
-        transform.position += (Vector3)currentDirection * moveSpeed * Time.deltaTime;
-    }
-
-    // Check if the vector represents diagonal movement.
-    bool IsDiagonal(Vector2 vector)
-    {
-        return Mathf.Approximately(vector.x, vector.y);
+        // Set the enemy's velocity to move only along x and y axes.
+        rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
     }
 }
--- M2A_Project/Assets/scripts/PlayerMovement.cs
+++ M2A_Project/Assets/scripts/PlayerMovement.cs
@@ -4,54 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust this value to set the movement speed.
+    public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
-    private Vector2 movementDirection = Vector2.zero;
+    private Vector2 movement;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    void Update()
     {
-        float moveX = 0f;
-        float moveY = 0f;
+        // Input handling for movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            moveY = 1f;
-            movementDirection = Vector2.up;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            moveY = -1f;
-            movementDirection = Vector2.down;
-        }
+        movement = new Vector2(horizontalInput, verticalInput).normalized;
+    }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveX = -1f;
-            movementDirection = Vector2.left;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            moveX = 1f;
-            movementDirection = Vector2.right;
-        }
-
-        Vector2 movement = new Vector2(moveX, moveY);
-
-        // If no movement key is pressed, stop moving.
-        if (movement == Vector2.zero)
-        {
-            rb.velocity = Vector2.zero;
-        }
-        // If a movement key is pressed, move in the direction of the first key pressed.
-        else if (movement == movementDirection)
-        {
-            rb.velocity = movement * moveSpeed;
-        }
+    void FixedUpdate()
+    {
+        // Move the player
+        rb.velocity = movement * moveSpeed;
     }
 }
