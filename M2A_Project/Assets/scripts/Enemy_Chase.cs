f44f95437910a59ee6e1e2d0c827cd035206d76d
CatEnjoyers <137115804+CatEnjoyers@users.noreply.github.com>
2023-10-02 17:01:51 -0400

    worked on stuff(broken ai)

--- M2A_Project/Assets/scripts/Enemy_Chase.cs
+++ M2A_Project/Assets/scripts/Enemy_Chase.cs
@@ -1,22 +1,19 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    public Transform player; // Reference to the player's transform.
-    public float moveSpeed = 3.0f; // Adjust this value to control the speed of the enemy.
-    private Rigidbody2D rb;
+    public Transform player;  // Reference to the player's transform.
+    private NavMeshAgent agent;
 
-    private void Start()
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        // Calculate the direction to move towards the player.
-        Vector2 direction = (player.position - transform.position).normalized;
-
-        // Set the enemy's velocity to move only along x and y axes.
-        rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+        // Set the destination to the player's position.
+        agent.SetDestination(player.position);
     }
 }
