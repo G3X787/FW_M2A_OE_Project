5e0bba1cff0b7e3fdedde11e43f4f80070b65f11
CatEnjoyers <137115804+CatEnjoyers@users.noreply.github.com>
2023-10-03 20:38:04 -0400

    added sprinting to movement sript
    added sprinting to movement script and did some stuff to the ai demo map to get a better understanding

--- M2A_Project/Assets/scripts/PlayerMovement.cs
+++ M2A_Project/Assets/scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f; // Speed when Shift is held down
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -21,11 +22,21 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         movement = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // Check if Shift is held down to increase speed
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? runSpeed : walkSpeed;
+        movement *= currentSpeed;
     }
 
     void FixedUpdate()
     {
         // Move the player
-        rb.velocity = movement * moveSpeed;
+        rb.velocity = movement;
+
+        // Check if the player is not moving and set velocity to zero to stop sliding
+        if (movement == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
